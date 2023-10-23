98a522927cc45cbf80e3780e535f8cc2dd1ff61a
Milos-GoodGuy <milos@goodguys.nu>
2023-10-23 12:24:15 +0200

    code edits

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,73 @@
 
 import '../main.scss'
 
-  
-const foo =(name)=>{
-  console.log(`aaa ${name}`)
-}
-
-foo('Mika')
-
 window.Webflow ||= [];
 window.Webflow.push(() =>{
+      
+  // Adding titles to images
+  $(document).ready(function(){
+      $('img').each(function(){
+        // take alt text of each image
+          let imageAltText = $(this).attr('alt');
+        // add as title to each image
+          $(this).attr('title', imageAltText);
+      });
+  });
 
-  console.log('mika');
-  
+    
   // hide default weglot language swither
   setTimeout(function(){ document.querySelector('.weglot-container').style.display = 'none'; }, 1000);
 
-  //show popup after page load
-  setTimeout(function(){ document.querySelector('.section_popup').style.display = 'flex'; }, 5000);
-	
-
-  // disable scroll when main menu is open
-  const body = document.body;
-  const navigationButton = document.getElementById("w-nav-button");
-  navigationButton.addEventListener('click', function (event) {
-    let areaExpanded = navigationButton.getAttribute("aria-expanded")
-    if (areaExpanded === 'false') {
-        body.style.overflow = 'hidden';
-    } else {
-        body.style.overflow = 'auto';
-    }
-   });
-  
-
-  // when user changes the language in the language swither that is inside main menu (tablet and mobile)
-  // we triger the hamburger button animation and enable page scroll again, since the menu dropdown closes
-  // after the user change the language
-  $('.wg-dropdown-1-link').click(function(){
-    let hamburgerButton = $('#main-menu-hamburger')
-    hamburgerButton.prop('checked', false);
-    body.style.overflow = 'auto';
+    
+  // here we are watching for style changes on themobile menu w-nav-overlay-0
+  // we also added a settimeout to check for this element every 500ms
+  // because he is not loaded on the page instantly
+  // when the display property is none we disable page scroll and set
+  // hamburger menu icon checkbox to true
+  // when the display property is not false we enable page skroll
+  // and set the hamburger checkbox to false
+  // this was implemented because the hamburger animation triger
+  // menu can close on every link click, language change and we want to monitor that
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if(mutation.attributeName === 'style'){
+        var targetNode = document.getElementById('w-nav-overlay-0');
+        if (targetNode) {
+          // Check if the 'display' style property has been changed
+          var currentDisplayStyle = targetNode.style.display;
+          var previousDisplayStyle = mutation.oldValue ? mutation.oldValue : '';
+          
+          if (currentDisplayStyle === 'none') {
+              let hamburgerButton = $('#main-menu-hamburger')
+              hamburgerButton.prop('checked', false);
+              document.body.style.overflow = 'auto';
+          } else {
+              let hamburgerButton2 = $('#main-menu-hamburger')
+              hamburgerButton2.prop('checked', true);
+              document.body.style.overflow = 'hidden';
+          }
+        }
+      }
+    });    
   });
+    
+  // Notify me of style changes
+  var observerConfig = {
+    attributes: true, 
+    attributeFilter: ["style"]
+  };
+  
+  function addObserverIfDesiredNodeAvailable() {
+    var targetNode = document.getElementById('w-nav-overlay-0');
+    if(!targetNode) {
+        //The node we need does not exist yet.
+        //Wait 500ms and try again
+        window.setTimeout(addObserverIfDesiredNodeAvailable,500);
+        return;
+    }
+    observer.observe(targetNode, observerConfig);
+  }
+  addObserverIfDesiredNodeAvailable();
+      
+
 });
